3102b4f348b43891d232c899bfd4db1a385ab175
cqlzn <cqlzn531@sina.cn>
2017-08-17 00:37:32 +0800

    Add files via upload

---
+++ models/get_models.m
@@ -0,0 +1,12 @@
+try
+    fprintf('Downloading A16.caffemodel...\n');
+    urlwrite('https://qllbwg.dm2302.livefilestore.com/y4mwMmQbjUNk5djf7KWFYpcjh7KFfq1DKx0eLUd9XIXFQXV8gg9IRutJaCLcKuMbCeZZqXWvKrVJPXCcrzpRbj_NeDF6PSqSAgj8H3KEz2y5PoflfiZ5yWosdUupEq5T-HxhQ2zOgB5tmgwf__LCTIWJ4ZU86b2Td8Qxs_EWXKI4ifQY_KRKvt_vuGwVWYZhmUChbFsyZlPoaR3NcFlDvcJWQ/A16.caffemodel?download&psid=1',...
+        'A16.caffemodel');
+    fprintf('Done\n');
+    fprintf('Downloading C128.caffemodel...\n');
+    urlwrite('https://qbluwg.dm2302.livefilestore.com/y4mELtMV4SkZTOy1iNmoQftLeuo7bzw_u-UcHxizmLPqDwIXy598ioCRco23jVHalDbtm5xE9WdIQDdQmu8fVpyWSExLNwjKvSXcSNEv-8MElWeolusHuqEesTM6ZrsQyNFzLZy-bfR3BCU055Q__Ao75gLvgqktnGiXS_hn_FCv7TGpO3sjf8IVzrNW9XH4uhNLBwwo6A7nk0sn747We5slg/C128.caffemodel?download&psid=1',...
+        'C128.caffemodel');
+    fprintf('Done\n');
+catch
+    fprintf('Error in downloading, please try links https://1drv.ms/f/s!AgdpPAZcyziFgb8DJ_q91aFI39KKPw');
+end
